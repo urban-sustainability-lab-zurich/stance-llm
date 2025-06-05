39ac135db44025d9a4a579b8b817fda2dda9e8b8
Mario Angst <mario.angst@gmail.com>
2025-06-05 17:46:54 +0200

    tests: removed commented out OpenAI test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,8 @@ import pytest
 from guidance import models
 import os
 from stance_llm.process import detect_stance
-from stance_llm.base import REGISTERED_LLM_CHAINS, CONSTRAINED_GRAMMAR_CHAINS
 
 # PREPARE EXAMPLE DATA ----------------
-
 
 @pytest.fixture(scope="module")
 def test_examples():
@@ -32,16 +30,7 @@ def test_examples():
     ]
     return test_egs
 
-
 # LOAD MODELS ------------
-
-
-# @pytest.fixture(scope="module")
-# def gpt35_openai():
-#     # OPENAI api key is loaded from a .env file in tests/ via pytest-dotenv
-#     gpt35 = models.OpenAI("gpt-3.5-turbo", api_key=os.environ["OPEN_AI_KEY"])
-#     return gpt35
-
 
 @pytest.fixture(scope="module")
 def gpt2_trf():
@@ -52,31 +41,7 @@ def gpt2_trf():
 
 # RUN STANCE DETECTIONS FOR ALL CHAINS --------------
 
-
-# @pytest.fixture(scope="module")
-# def allowed_openai_chains():
-#     allowed = [
-#         chain
-#         for chain in REGISTERED_LLM_CHAINS
-#         if chain not in CONSTRAINED_GRAMMAR_CHAINS
-#     ]
-#     return allowed
-
-
 # non-masked, iterate across all chains
-
-
-# @pytest.fixture(scope="module")
-# def stance_detection_runs_openai(test_examples, gpt35_openai, allowed_openai_chains):
-#     classifications = []
-#     for chain in allowed_openai_chains:
-#         for test_eg in test_examples:
-#             classification = detect_stance(
-#                 eg=test_eg, llm=gpt35_openai, chain_label=chain
-#             )
-#             classifications.append(classification)
-#     return classifications
-
 
 @pytest.fixture(scope="module")
 def stance_detection_runs_trf(test_examples, gpt2_trf):
@@ -89,20 +54,7 @@ def stance_detection_runs_trf(test_examples, gpt2_trf):
             classifications.append(classification)
     return classifications
 
-
 # masked, single chain as example
-
-
-# @pytest.fixture(scope="module")
-# def stance_detection_run_masked_openai(test_examples, gpt35_openai):
-#     run = detect_stance(
-#         eg=test_examples[0],
-#         llm=gpt35_openai,
-#         chain_label="s2is",
-#         entity_mask="Organisation X",
-#     )
-#     return run
-
 
 @pytest.fixture(scope="module")
 def stance_detection_run_masked_trf(test_examples, gpt2_trf):
@@ -116,7 +68,6 @@ def stance_detection_run_masked_trf(test_examples, gpt2_trf):
 
 
 # PROCESSING TESTS
-
 
 @pytest.fixture(scope="module")
 def test_output_dir():
